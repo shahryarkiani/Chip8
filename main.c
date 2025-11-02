4528647de61406acd44cd7d744907d52a93bfa54
shahryarkiani <shahryarkiani04@gmail.com>
2025-11-02 11:43:19 -0800

    Basic window working, set up Chip8 state struct

--- main.c
+++ main.c
@@ -1,5 +1,80 @@
 #include <windows.h>
 
-int WINAPI wWinMain(HINSTANCE hInst, HINSTANCE hInstPrev, LPSTR lpCmdLine, int nCmdShow) {
-	return 0;
+#define CHIP8_SCREEN_HEIGHT (32)
+#define CHIP8_SCREEN_WIDTH  (64)
+
+typedef struct {
+    UINT8* MEM;     // Memory
+    UINT16 I;       // Address Register
+    UINT8 REG[16];  // General Purpose Registers
+    UINT16 PC;      // Program Counter
+    UINT16 DTIMER;  // Delay Timer
+    UINT16 STIMER;  // Sound Timer
+} Chip8State;
+
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+{
+    // Register the window class.
+    const wchar_t CLASS_NAME[] = L"CHIP8-EMULATOR";
+
+    WNDCLASS wc = {0};
+
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+
+    HWND hwnd = CreateWindowEx(
+        0,                              // Optional window styles.
+        CLASS_NAME,                     // Window class
+        CLASS_NAME,                     // Window text
+        WS_OVERLAPPEDWINDOW,            // Window style
+        // Size and position
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+        NULL,       // Parent window    
+        NULL,       // Menu
+        hInstance,  // Instance handle
+        NULL        // Additional application data
+    );
+
+    if (hwnd == NULL) return 0xDEADBEEF;
+
+    ShowWindow(hwnd, nCmdShow);
+
+    MSG msg = {0};
+    while (GetMessage(&msg, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+        case WM_DESTROY:
+        {
+            PostQuitMessage(0);
+            return 0;
+        }
+        case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hwnd, &ps);
+
+            FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+
+            EndPaint(hwnd, &ps);
+            return 0;
+        }
+    }
+
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
