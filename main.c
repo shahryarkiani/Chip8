19e0428520a513ed4820c714442811c49a73a828
shahryarkiani <shahryarkiani04@gmail.com>
2025-11-03 00:01:33 -0800

    Mostly working, just need sound

--- main.c
+++ main.c
@@ -1,6 +1,31 @@
 #include <windows.h>
+
 #define CHIP8_SCREEN_HEIGHT (32)
 #define CHIP8_SCREEN_WIDTH  (64)
+#define PROGRAM_START 0x200
+#define MEMORY_SIZE 4096
+
+#define INSTRUCTIONS_PER_TICK 10
+#define MEM_AFFECT_I TRUE
+
+const char fontData[] = {
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+};
 
 LRESULT CALLBACK windowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
@@ -13,8 +38,8 @@ typedef struct {
     UINT16  I;          // Address Register
     UINT8   REG[16];    // General Purpose Registers
     UINT16  PC;         // Program Counter
-    UINT16  DTIMER;     // Delay Timer
-    UINT16  STIMER;     // Sound Timer
+    UINT8  DTIMER;      // Delay Timer
+    UINT8  STIMER;      // Sound Timer
     UINT16  STACK[8];   // PC Stack
     UINT8   SP;         // Stack Pointer
 } Chip8State;
@@ -43,6 +68,10 @@ Framebuffer chipScreen = {
 
 KeyInfo keyInfo = { 0 };
 
+// Needed to block for a key press
+static UINT8 pressedKeyCode = 0xFF;
+static BOOL needsKeyCode = FALSE;
+
 UINT8 mapKeyCode(WPARAM virtualKeyCode) {
     switch (virtualKeyCode) {
         case 0x31: return 0;  // ('1') -> 0
@@ -55,15 +84,20 @@ UINT8 mapKeyCode(WPARAM virtualKeyCode) {
         case 0x52: return 7;  // ('R') -> 7
         case 0x41: return 8;  // ('A') -> 8
         case 0x53: return 9;  // ('S') -> 9
-        case 0x44: return 11; // ('D') -> 10
-        case 0x46: return 12; // ('F') -> 11
-        case 0x5A: return 13; // ('Z') -> 12
-        case 0x58: return 14; // ('X') -> 13
-        case 0x43: return 15; // ('C') -> 14
-        case 0x56: return 16; // ('V') -> 15
+        case 0x44: return 10; // ('D') -> 10
+        case 0x46: return 11; // ('F') -> 11
+        case 0x5A: return 12; // ('Z') -> 12
+        case 0x58: return 13; // ('X') -> 13
+        case 0x43: return 14; // ('C') -> 14
+        case 0x56: return 15; // ('V') -> 15
     }
 
     return 0xFF;
+}
+
+void updateTimers(Chip8State* chipState) {
+    if (chipState->DTIMER) --chipState->DTIMER;
+    if (chipState->STIMER) --chipState->STIMER;
 }
 
 int updateState(Chip8State* chipState) {
@@ -82,8 +116,7 @@ int updateState(Chip8State* chipState) {
                 
             if (lower8 == 0xE0) { // Clear Screen
                 memset(chipScreen.pixels, 0, CHIP8_SCREEN_HEIGHT * CHIP8_SCREEN_WIDTH * 4);
-            }
-            else if (lower8 == 0xEE) { // Return from subroutine call
+            } else if (lower8 == 0xEE) { // Return from subroutine call
                 --chipState->SP;
                 chipState->PC = chipState->STACK[chipState->SP];
             }
@@ -172,29 +205,90 @@ int updateState(Chip8State* chipState) {
             chipState->PC = chipState->REG[0] + lower12;
         } break;
         case 0xC: { // TODO: make this more random
-            chipState->REG[reg1] = 0xAB;
+            chipState->REG[reg1] = (0xAB * (chipState->REG[reg1] ^ chipState->PC) * 17) % 255;
         } break;
         case 0xD: {
-            int x = chipState->REG[reg1];
-            int y = chipState->REG[reg2];
+            int x = chipState->REG[reg1] % 64;
+            int y = chipState->REG[reg2] % 32;
             int n = lower4;
-            for (int ypos = y; ypos < y + n; ypos++) {
-                // TODO   
+            chipState->REG[0xF] = 0;
+            for (int ypos = y; ypos < y + n && ypos < 32; ypos++) {
+                UINT8 data = chipState->MEM[chipState->I + (ypos - y)];
+                UINT8 mask = 0x80;
+                for (int xpos = 0; xpos < 8 && x + xpos < 64; xpos++) {
+                    BOOL color = (data & mask);
+                    mask >>= 1;
+                    UINT8 xorVal = color ? 255 : 0;
+                    int pixelIdx = ypos * 64 * 4 + (x + xpos) * 4;
+                    if (color && chipScreen.pixels[pixelIdx]) chipState->REG[0xF] = 1;
+                    chipScreen.pixels[pixelIdx + 0] ^= xorVal;
+                    chipScreen.pixels[pixelIdx + 1] ^= xorVal;
+                    chipScreen.pixels[pixelIdx + 2] ^= xorVal;
+                    chipScreen.pixels[pixelIdx + 3] ^= xorVal;
+                }
             }
         } break;
         case 0xE: {
             BOOL keyPressed = keyInfo.isKeyDown[chipState->REG[reg1]];
             if (lower8 == 0x9E && keyPressed) {
                 chipState->PC += 2;
-            }
-            else if (lower8 == 0xA1 && !keyPressed) {
+            } else if (lower8 == 0xA1 && !keyPressed) {
                 chipState->PC += 2;
             }
         } break;
-        case 0xF: { // TODO
+        case 0xF: {
             switch (lower8) {
                 case 0x7: {
-
+                    chipState->REG[reg1] = chipState->DTIMER;
+                } break;
+                case 0xA: {// Use globals to communicate with event processor
+                    // 2 variables, keyCode = 0xFF and needsKey = False
+                    // when this needs a value, it sets needsKey to True, and doesn't update the PC, and does this
+                    // in a loop while we wait for input
+                    // the input processing sets keyCode to the first keyDown it gets IF needsKey == True
+                    // then when this instruction runs again it can consume the keyCode, clear it and set needsKey to false
+                    if (pressedKeyCode != 0xFF) {
+                        needsKeyCode = FALSE;
+                        chipState->REG[reg1] = pressedKeyCode;
+                        pressedKeyCode = 0xFF;
+                    }
+                    else {
+                        needsKeyCode = TRUE;
+                    }
+                } break;
+                case 0x15: {
+                    chipState->DTIMER = chipState->REG[reg1];
+                } break;
+                case 0x18: {
+                    chipState->STIMER = chipState->REG[reg1];
+                } break;
+                case 0x1E: {
+                    chipState->I += chipState->REG[reg1];
+                } break;
+                case 0x29: { 
+                    chipState->I = chipState->REG[reg1] * 5;
+                } break;
+                case 0x33: {
+                    int x = chipState->REG[reg1];
+                    for (int i = 0; i < 3; i++) {
+                        UINT8 digit = x % 10;
+                        x /= 10;
+                        chipState->MEM[chipState->I + (2 - i)] = digit;
+                    }
+                } break;
+                case 0x55: {
+                    int x = reg1;
+                    for (int i = 0; i <= x; i++) {
+                        chipState->MEM[chipState->I + i] = chipState->REG[i];
+                    }
+                    if (MEM_AFFECT_I) chipState->I += x + 1;
+                } break;
+                case 0x65: {
+                    int x = reg1;
+                    for (int i = 0; i <= x; i++) {
+                        chipState->REG[i] = chipState->MEM[chipState->I + i];
+                    }
+                    if (MEM_AFFECT_I) chipState->I += x + 1;
                 } break;
             }
         } break;
@@ -205,9 +299,52 @@ int updateState(Chip8State* chipState) {
     return 0;
 }
 
+void loadProgram(Chip8State* chipState, HWND hwnd) {
+    HANDLE hFile = CreateFileA(
+        "airplane.ch8",
+        GENERIC_READ,
+        FILE_SHARE_READ,
+        NULL,
+        OPEN_EXISTING,
+        FILE_ATTRIBUTE_NORMAL,
+        NULL
+    );
+
+    if (hFile == INVALID_HANDLE_VALUE) {
+        MessageBoxA(hwnd, "Failed to open ROM file!", NULL, MB_ICONERROR);
+        PostQuitMessage(0);
+        return;
+    }
+
+    DWORD fileSize = GetFileSize(hFile, NULL);
+    if (fileSize == INVALID_FILE_SIZE) {
+        MessageBoxA(hwnd, "Failed to get file size!", NULL, MB_ICONERROR);
+        CloseHandle(hFile);
+        PostQuitMessage(0);
+        return;
+    }
+
+    BOOL success = ReadFile(
+        hFile,
+        chipState->MEM + PROGRAM_START,
+        fileSize,
+        NULL,
+        NULL
+    );
+    CloseHandle(hFile);
+
+    if (!success) {
+        MessageBoxW(hwnd, L"Failed to read file!", NULL, MB_ICONERROR);
+        PostQuitMessage(0);
+        return;
+    }
+
+    memcpy(chipState->MEM, fontData, 80);
+}
+
 void gameLoop(HWND hwnd) {
     Chip8State chipState = {
-        .MEM = malloc(4096),
+        .MEM = malloc(MEMORY_SIZE),
         .I = 0,
         .REG = {0},
         .PC = 0x200,
@@ -217,7 +354,7 @@ void gameLoop(HWND hwnd) {
         .SP = 0
     };
 
-    int j = 0;
+    loadProgram(&chipState, hwnd);
 
     for (;;) {
         MSG msg;
@@ -231,18 +368,15 @@ void gameLoop(HWND hwnd) {
         }
 
         // Update Game State
-        updateState(&chipState);
+        for (int i = 0; i < INSTRUCTIONS_PER_TICK; i++)
+            updateState(&chipState);
+        // Update Timer States
+        updateTimers(&chipState);
 
-        // Update Screen
-        for (int i = 0; i < 64; i += 4) {
-            chipScreen.pixels[i] = keyInfo.isKeyDown[i / 4] * 0xFF;
-            chipScreen.pixels[i + 1] = keyInfo.isKeyDown[i / 4] * 0xFF;
-            chipScreen.pixels[i + 2] = keyInfo.isKeyDown[i / 4] * 0xFF;
-            chipScreen.pixels[i + 3] = 0xFF;
-        }
-
+        // TODO: maybe need to update screen after every DRAW instruction
+        // not just after a batch of instructions
         InvalidateRect(hwnd, NULL, FALSE);
-        Sleep(16);
+        Sleep(15); // TODO: Adjust sleep based on how long the emulation updates took
     }
 
     free(chipState.MEM);
@@ -253,12 +387,13 @@ LRESULT CALLBACK windowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
         case WM_KEYDOWN: {
             UINT8 keyCode = mapKeyCode(wParam);
             if (keyCode == 0xFF) return 0;
+            if (needsKeyCode) pressedKeyCode = keyCode;
             keyInfo.isKeyDown[keyCode] = TRUE;
             return 0;
         }
         case WM_KEYUP: {
             UINT8 keyCode = mapKeyCode(wParam);
-            if (keyCode == 0xFF) return;
+            if (keyCode == 0xFF) return 0;
             keyInfo.isKeyDown[keyCode] = FALSE;
             return 0;
         }
